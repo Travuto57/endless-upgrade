ac3a78e93b695d68f6c8f5eef6bd949530932ac9
= <team.asl398@gmail.com>
2025-10-24 22:23:17 +0530

    Uncommenting authentication code

--- server/routers.ts
+++ server/routers.ts
@@ -21,7 +21,7 @@ export const appRouter = router({
   }),
 
   giveaway: router({
-    createEntry: publicProcedure
+    createEntry: protectedProcedure
       .input(z.object({
         firstName: z.string().min(1),
         lastName: z.string().min(1),
